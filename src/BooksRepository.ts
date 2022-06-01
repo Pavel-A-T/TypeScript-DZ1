82f5b2c4de4e11ff3e965b5cc3c0c9e90d751da4
Pavel Titov <otrepiev.grishka@yandex.ru>
2022-06-01 18:51:52 +0300

    пока только так могу типизировать

--- src/BooksRepository.ts
+++ src/BooksRepository.ts
@@ -1,18 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const formData = require("express-form-data");
-
 let books: Array<Book> = [];
-
-const stor = {
-   books
-};
-
-//. createBook(book){} - создание книги
-// 2. getBook(id){} - получение книги по id
-// 3. getBooks(){} - получение всех книг
-// 4. updateBook(id){} - обновление книги
-// 5. deleteBook(id){}
 
 [1, 2, 3].map(el => {
     const newBook: Book = {
@@ -23,83 +9,46 @@ const stor = {
         fileName:"",
         id:""
     }
-    stor.books.push(newBook);
+    books.push(newBook);
 });
 
-
-const app = express();
-app.use(formData.parse());
-app.use(cors());
-
-app.get('/api/books', (req:Request, res:Response) => {
-    const {books} = stor;
-    res.json(books);
-});
-
-app.get('/api/books/:id', (req:Request, res:Response) => {
-    const {books} = stor;
-    const {id} = req.params;
-    const index = books.findIndex(el => el.id === id);
-
-    if (index !== -1) {
-        res.json(books[index]);
-    } else {
-        res.status(404);
-        res.json(`Book id=${id} not found`);
-    }
-});
-
-app.post('/api/books', (req:Request, res:Response) => {
-    const {title, description, favorite,
-        fileCover, fileName} = req.body;
-    const {books} = stor;
-    const book = new Book(title, description, favorite, fileCover, fileName);
+//createBook(book){} - создание книги
+function createBook(book:Book):Book{
     books.push(book);
-    res.status(201);
-    res.json(book);
-});
+    return book;
+}
 
-
-app.put('/api/books/:id', (req:Request, res:Response) => {
-    const {books} = stor;
-    const {id} = req.params;
+//getBook(id){} - получение книги по id
+function getBook(id:string):Book{
     const index = books.findIndex(el => el.id === id);
-
     if (index !== -1) {
-        let book = books[index];
-        const {title, description, favorite,
-            fileCover, fileName} = req.body;
-        book = {
-            ...books[index],
-            title,
-            description,
-            favorite,
-            fileCover,
-            fileName
-        };
-        res.json(book);
+        return  books[index];
     } else {
-        res.status(404);
-        res.json(`Book id=${id} not found`);
+        return null;
     }
+}
 
-});
+// 3. getBooks(){} - получение всех книг
+function getBooks():string{
+   return JSON.stringify(books);
+}
 
-app.delete('/api/books/:id', (req:Request, res:Response) => {
-    const {books} = stor;
-    const {id} = req.params;
+// 4. updateBook(book:Book){} - обновление книги
+function updateBook(book:Book):boolean{
+    const index = books.findIndex(el => el.id === book.id);
+    if (index !== -1) {
+        books[index] = book;
+        return true;
+    }
+    return false;
+}
+
+// 5. deleteBook(id){}
+function deleteBook(id:string):boolean{
     const index = books.findIndex(el => el.id === id);
-
     if (index !== -1) {
         books.splice(index, 1);
-        res.json('ok');
-    } else {
-        res.status(404);
-        res.json(`Book id=${id} not found`);
+        return true;
     }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    return false;
+ }
